4f9d12983ec77557b37d5c2ded213dfa7139e58c
Mahnoor Shafique <mahnoorshafique623@gmail.com>
2023-05-31 17:24:52 +0500

    added gitignore

--- githubresource/terraform.tf
+++ githubresource/terraform.tf
@@ -1,7 +1,7 @@
 #any cloud provider
 
 provider "github"{
- token=
+ token="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"
 }
 #name repo in github will be created using name
 #terraform-first-repo terraform will acess this repo in code using this name
